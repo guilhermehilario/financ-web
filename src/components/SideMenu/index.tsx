0ce63549c000f2ec85ef1c4d5f9d1ed07f84d942
guilhermehilario <guilhermehilariosilva@gmail.com>
2022-04-03 21:29:04 -0300

    feat: create dashboard structure

---
+++ src/components/SideMenu/index.tsx
@@ -0,0 +1,23 @@
+import { Container } from './styles';
+
+export const SideMenu = (): JSX.Element => (
+  <Container>
+    <div>
+      <span></span>
+    </div>
+    <div>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+    <div>
+      <span></span>
+    </div>
+  </Container>
+);
---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,8 @@
+import { Container } from './styles';
+import { SideMenu } from '../../components/SideMenu';
+
+export const Dashboard = (): JSX.Element => (
+  <Container>
+    <SideMenu />
+  </Container>
+);
