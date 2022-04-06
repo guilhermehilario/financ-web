25bb51155b33de2da39ef910367795cf396dc338
will-souza97 <rzy.x97@gmail.com>
2022-04-05 22:02:33 -0300

    style: styling SideMenu

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 // import { Avatar } from './avartar';
+import { Text } from '../Text';
 import * as styles from './styles';
 
 export const SideMenu: React.FC = () => (
@@ -9,15 +10,47 @@ export const SideMenu: React.FC = () => (
       src="https://avatars.githubusercontent.com/u/54961000?v=4"
       alt=""
     />
-    <p>William de Souza</p>
+
+    <Text fontSize="normal" color="secondary" bold>
+      William de Souza
+    </Text>
 
     <styles.NavList>
-      <styles.ItemList>Dashboard</styles.ItemList>
-      <styles.ItemList>Wallets</styles.ItemList>
-      <styles.ItemList>Calendar</styles.ItemList>
-      <styles.ItemList>Analytics</styles.ItemList>
-      <styles.ItemList>Learn</styles.ItemList>
-      <styles.ItemList>Settings</styles.ItemList>
+      <styles.ItemList>
+        <Text fontSize="medium" color="text" bold>
+          <styles.Link to="/">Dashboard</styles.Link>
+        </Text>
+      </styles.ItemList>
+
+      <styles.ItemList>
+        <Text fontSize="medium" color="text" bold>
+          <styles.Link to="/">Wallets</styles.Link>
+        </Text>
+      </styles.ItemList>
+
+      <styles.ItemList>
+        <Text fontSize="medium" color="text" bold>
+          <styles.Link to="/">Calendar</styles.Link>
+        </Text>
+      </styles.ItemList>
+
+      <styles.ItemList>
+        <Text fontSize="medium" color="text" bold>
+          <styles.Link to="/">Analytics</styles.Link>
+        </Text>
+      </styles.ItemList>
+
+      <styles.ItemList>
+        <Text fontSize="medium" color="text" bold>
+          <styles.Link to="/">Learn</styles.Link>
+        </Text>
+      </styles.ItemList>
+
+      <styles.ItemList>
+        <Text fontSize="medium" color="text" bold>
+          <styles.Link to="/">Settings</styles.Link>
+        </Text>
+      </styles.ItemList>
     </styles.NavList>
 
     <button type="submit">Logout</button>
--- src/components/SideMenu/styles.ts
+++ src/components/SideMenu/styles.ts
@@ -1,29 +1,41 @@
+import { Link as Linking } from 'react-router-dom';
+
+import { lighten } from 'polished';
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
   background-color: ${({ theme }) => theme.colors.shape};
-  border-radius: 0 20px 20px 0px;
+  border-radius: 0 20px 20px 0;
   width: 25vw;
   height: 100vh;
+  padding: 32px;
 `;
 
 export const Avatar = styled.img`
   border-radius: 20%;
-  margin: 15px 0 10px 5px;
-  width: 20%;
+  margin: 32px 0 16px;
+  width: 25%;
   border: 2px solid ${({ theme }) => theme.colors.shape_dark};
 `;
 
 export const NavList = styled.ul`
-  font-size: 20px;
+  margin-top: 32px;
   list-style: none;
-  padding: 10px;
   width: 100%;
   height: 50%;
 `;
 
 export const ItemList = styled.li`
-  margin: 10px 0 10px 5px;
-  padding: 10px 0;
-  width: 80%;
+  padding: 16px 0;
+  width: 100%;
+  border-bottom: 2px solid
+    ${({ theme }) => lighten(0.3, theme.colors.text_light)};
+`;
+
+export const Link = styled(Linking)`
+  color: inherit;
+  text-decoration: none;
 `;
