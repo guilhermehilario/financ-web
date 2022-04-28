5747d780cd291ab5ce14afadf6da009df0c90b63
guilhermehilario <guilhermehilariosilva@gmail.com>
2022-04-27 22:56:01 -0300

    feat: configuring firebase

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 
 import { FabButton } from '../../components/feature-components/FabButton';
 import { HeaderWrapper as Header } from '../../components/feature-components/Header';
 import { MainWrapper as Main } from '../../components/feature-components/Main';
+import { app } from '../../google-services';
 import { Container } from './styles';
 
-export const Dashboard: React.FC = () => (
-  <Container>
-    <Header title="Dashboard" />
-    <Main />
-    <FabButton />
-  </Container>
-);
+const db = getFirestore(app);
+
+export const Dashboard: React.FC = () => {
+  const getWallets = async () => {
+    const wallets = await getDocs(collection(db, 'wallets'));
+    wallets.forEach(doc => console.log(doc.data()));
+    console.log(wallets);
+  };
+
+  useEffect(() => {
+    getWallets();
+  });
+
+  return (
+    <Container>
+      <Header title="Dashboard" />
+      <Main />
+      <FabButton />
+    </Container>
+  );
+};
