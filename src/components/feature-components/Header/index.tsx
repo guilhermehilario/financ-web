b761fc9ef732607b34697c1cdefc7021b7f5d2a0
guilhermehilario <guilhermehilariosilva@gmail.com>
2022-05-01 19:25:12 -0300

    feat: get data from firebase

--- src/components/feature-components/Header/index.tsx
+++ src/components/feature-components/Header/index.tsx
@@ -4,35 +4,39 @@ import { Card } from '../Card';
 import { Container, Title, Header, Section } from './styles';
 import { HeaderProps } from './types';
 
-export const HeaderWrapper = ({ title }: HeaderProps) => (
-  <Container>
-    <Header>
-      <Title fontSize="normal" color="white">
-        {title}
-      </Title>
-    </Header>
-    <Section>
-      <Card
-        title="Banco1"
-        currency="BRL"
-        income={1000}
-        outcome={600}
-        value={400}
-      />
-      <Card
-        title="Banco1"
-        currency="BRL"
-        income={1000}
-        outcome={600}
-        value={400}
-      />
-      <Card
-        title="Banco1"
-        currency="BRL"
-        income={1000}
-        outcome={600}
-        value={400}
-      />
-    </Section>
-  </Container>
-);
+export const HeaderWrapper = ({ title, cWallets }: HeaderProps) => {
+  console.log(cWallets);
+
+  return (
+    <Container>
+      <Header>
+        <Title fontSize="normal" color="white">
+          {title}
+        </Title>
+      </Header>
+      <Section>
+        <Card
+          title="Banco1"
+          currency="BRL"
+          income={1000}
+          outcome={600}
+          value={400}
+        />
+        <Card
+          title="Banco1"
+          currency="BRL"
+          income={1000}
+          outcome={600}
+          value={400}
+        />
+        <Card
+          title="Banco1"
+          currency="BRL"
+          income={1000}
+          outcome={600}
+          value={400}
+        />
+      </Section>
+    </Container>
+  );
+};
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { collection, getDocs, getFirestore } from 'firebase/firestore/lite';
 
 import { FabButton } from '../../components/feature-components/FabButton';
 import { HeaderWrapper as Header } from '../../components/feature-components/Header';
@@ -10,20 +10,32 @@ import { Container } from './styles';
 
 const db = getFirestore(app);
 
+// interface WalletProps {
+//   title: string;
+//   outcome: number;
+//   income: number;
+//   balance: number;
+//   user: string;
+// }
+
 export const Dashboard: React.FC = () => {
+  const [walletsList, setWalletsList] = useState<any>();
+
   const getWallets = async () => {
     const wallets = await getDocs(collection(db, 'wallets'));
-    wallets.forEach(doc => console.log(doc.data()));
-    console.log(wallets);
+    wallets.forEach(doc => setWalletsList(doc.data()));
+    console.count();
   };
 
   useEffect(() => {
-    getWallets();
-  });
+    (async () => {
+      await getWallets();
+    })();
+  }, [walletsList]);
 
   return (
     <Container>
-      <Header title="Dashboard" />
+      <Header title="Dashboard" cWallets={walletsList} />
       <Main />
       <FabButton />
     </Container>
